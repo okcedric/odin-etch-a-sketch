20e550092f5ac23a3539fdbc22a099fdfe7d2d2e
pr0pr3 <salut@tchang.fr>
2021-12-09 20:37:08 +0100

    Make a reset button

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-let pixelGrid = document.querySelector('#pixel-grid');
+let main = document.querySelector('main');
 
-let lineLength = 16;
-pixelGrid.style.cssText = `grid-template-rows: repeat(${lineLength},1fr); grid-template-columns: repeat(${lineLength},1fr);`;
-for(let i = 0; i< (lineLength*lineLength); i++){
-    let pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    pixelGrid.appendChild(pixel);
+
+function createGrid(lineLength){
+    let pixelGrid = document.createElement('div');
+    pixelGrid.classList.add('pixel-grid');
+    pixelGrid.style.cssText = `grid-template-rows: repeat(${lineLength},1fr); grid-template-columns: repeat(${lineLength},1fr);`;
+    for(let i = 0; i< (lineLength*lineLength); i++){
+        let pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixelGrid.appendChild(pixel);
+    }
+    main.appendChild(pixelGrid);
+
+    let pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        console.log('hey');
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.cssText = " background-color: #333;"
+        })
+    })
+
 }
 
+createGrid(16);
 
-let pixels = document.querySelectorAll('.pixel');
 
-pixels.forEach(pixel => {
-    console.log('hey');
-    pixel.addEventListener('mouseover', () =>{
-        pixel.style.cssText = " background-color: tomato;" 
-    })
+let resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener('click',()=>{
+    let pixelGrid = document.querySelector('.pixel-grid');
+    pixelGrid.remove();
+    let lineLength = false;
+    while(!lineLength || lineLength > 100 || lineLength < 0 || !(Number.isInteger(lineLength))) {
+        lineLength = parseInt(prompt('How many squares per slide ? ( Max: 100)'));
+    }
+    createGrid(lineLength);
 })
