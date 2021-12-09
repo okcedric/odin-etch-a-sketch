38d84027426ede6220fb9ea3bbf67d719c26fac2
pr0pr3 <salut@tchang.fr>
2021-12-09 19:37:03 +0100

    Make a variable number grid

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+let pixelGrid = document.querySelector('#pixel-grid');
+
+let lineLength = 16;
+pixelGrid.style.cssText = `grid-template-rows: repeat(${lineLength},1fr); grid-template-columns: repeat(${lineLength},1fr);`;
+for(let i = 0; i< (lineLength*lineLength); i++){
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixelGrid.appendChild(pixel);
+}
+
